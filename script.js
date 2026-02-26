492d7c2d837acea0f192e1604f8022de007ae0fd
Harammmkhan <haramkhankhan100@gmail.com>
2026-02-26 22:09:07 +0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let darkModeBtn = document.getElementById("darkModeBtn");
-    darkModeBtn.addEventListener("click", function() {
-        document.body.classList.toggle("dark-mode");
-    });
-});
